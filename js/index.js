4e5a3fcb28b2b4d05df00ad0881a44bbdbd059df
KSJ <fly881004@gmail.com>
2022-11-03 13:43:23 +0900

    코드 최적화중

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
-var readmore_on = false;
-var skills_show = false;
-var modal_on = false;
-var modal_youtube_on = false;
+
 var nav_mobile_on = false;
 
-var modal = document.getElementsByClassName("modal");
 var header = document.getElementsByClassName("header");
-var moon = document.getElementsByClassName("header_shutter");
 var header_title = document.getElementsByClassName("header_title");
 
 var nav = document.getElementsByClassName("nav");
@@ -19,12 +14,7 @@ var myinfo_introduce2 = document.getElementsByClassName("myinfo_introduce2");
 var myinfo_introduce2_img = document.getElementsByClassName(
   "myinfo_introduce2_img"
 );
-var skills_container = document.getElementsByClassName("skills_container");
-var skills_box = document.getElementsByClassName("skills_skillbox");
-var skills_title = document.getElementsByClassName("skills_title");
-var skills_text = document.getElementsByClassName("skills_text");
-var skills_img = document.getElementsByClassName("skills_img");
-var skills_readmore = document.getElementsByClassName("skills_readmore");
+
 var project_container = document.getElementsByClassName("project_container");
 var project_img1 = document.getElementsByClassName("project_img1");
 var project_intro1 = document.getElementsByClassName("project_intro1");
@@ -32,10 +22,6 @@ var project_img2 = document.getElementsByClassName("project_img2");
 var project_intro2 = document.getElementsByClassName("project_intro2");
 var project_img3 = document.getElementsByClassName("project_img3");
 var project_intro3 = document.getElementsByClassName("project_intro3");
-var footer = document.getElementsByClassName("footer");
-var fa_sms = document.getElementsByClassName("fa-sms");
-var fa_github = document.getElementsByClassName("fa-github");
-var fa_instagram = document.getElementsByClassName("fa-instagram");
 
 function nav_scroll(e) {
   // Nav_menu 스크롤 이동
@@ -69,49 +55,7 @@ function nav_mobile() {
   }
 }
 
-/* Youtube Api*/
-var tag = document.createElement("script");
-tag.src = "https://www.youtube.com/444";
-var firstScriptTag = document.getElementsByTagName("script")[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var player;
-/* Youtube Api end*/
 
-function youtube_on(e) {
-  if (modal_on == false) {
-    modal_on = true;
-    modal[0].style.display = "flex";
-  }
-  if (e.length > 0) {
-    this.youtube_link = e + "";
-  }
-  player = new YT.Player("player", {
-    height: "1080",
-    width: "1920",
-    videoId: e,
-    events: {
-      onReady: onPlayerReady,
-      onStateChange: onPlayerStateChange
-    }
-  });
-  function onPlayerReady(event) {
-    event.target.setVolume(10);
-    event.target.setPlaybackQuality("hd720");
-    event.target.playVideo();
-  }
-  function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.BUFFERING) {
-      event.target.setPlaybackQuality("hd720"); // <-- WORKS!
-    }
-  }
-}
-function modal_off() {
-  if (modal_on == true) {
-    modal[0].style.display = "none";
-    modal_on = false;
-  }
-  player.destroy(); // 유튜브 객체 삭제(페이지 렉 방지)
-}
 function github_on(e) {
   window.open("https://github.com/" + e);
 }
